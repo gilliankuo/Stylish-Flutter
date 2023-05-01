dcf7166d65d6dcf9ca28fd08ea0c9d8a909dd181
Gillian <gillian.kuo@snapask.com>
2023-05-01 19:52:27 +0800

    add AppWorks School offices location

---
+++ lib/map/model/office.dart
@@ -0,0 +1,8 @@
+class Office {
+  final String name;
+  final double lat;
+  final double lng;
+  final String address;
+
+  Office(this.name, this.lat, this.lng, this.address);
+}
--- lib/map/view/map_page.dart
+++ lib/map/view/map_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+import '../../util/app_bar.dart';
+import '../model/office.dart';
+
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
 
@@ -11,31 +14,43 @@ class MapPage extends StatefulWidget {
 class _MapPageState extends State<MapPage> {
   late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(25.038946886956325, 121.53239255767078);
+  final Map<String, Marker> _markers = {};
+  final List<Office> appWorksSchoolOffice = [
+    Office("AppWorks School", 25.038946886956325, 121.53239255767078, "AppWorks School"),
+    Office("AppWorks School 市府舊基地", 25.043765740367277, 121.56463102131549, "AppWorks School 市府舊基地")
+  ];
+  final LatLng _center = const LatLng(25.042756233286333, 121.54369880190039);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    setState(() {
+      _markers.clear();
+      for (final office in appWorksSchoolOffice) {
+        final marker = Marker(
+          markerId: MarkerId(office.name),
+          position: LatLng(office.lat, office.lng),
+          infoWindow: InfoWindow(
+            title: office.name,
+            snippet: office.address,
+          ),
+        );
+        _markers[office.name] = marker;
+      }
+    });
+
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.green[700],
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Maps Sample App'),
-          elevation: 2,
+    return Scaffold(
+      appBar: createStylishAppBar(context),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 13.0,
         ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
+        markers: _markers.values.toSet(),
       ),
     );
   }
